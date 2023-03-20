e376d1f0ca74657effb5f6c51e0614c36eb80ab7
emptykerr <104610868+emptykerr@users.noreply.github.com>
2023-03-20 23:07:53 +1300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/ui/home_page.dart';
+import 'package:to_do_app/ui/theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+        theme: Themes.light,
+        darkTheme: Themes.dark,
+        themeMode: ThemeMode.light,
+
+
+      home: HomePage()
+    );
+  }
+}
